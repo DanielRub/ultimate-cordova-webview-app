7bc7ac1f731f41c41da6ecf0e6941fe0551e9e33
Daniel RUBAMBURA <2379961@AFRICA.AIRTEL.ITM>
2019-08-05 07:31:24 +0100

    'test'

--- www/js/index.js
+++ www/js/index.js
@@ -58,7 +58,7 @@ function onOffline() {
 }
 
 function openBrowser() {
-   var url = 'https://dusthqdenevents.frb.io/';
+   var url = 'http://cloud4press.com:8080/avalon/';
    var target = '_blank';
    var options = "location=no,toolbar=no"
    var ref = cordova.InAppBrowser.open(url, target, options);
